b4a1129e83d14f37f8a3f43714d72c2556786826
valentinrymsha <valentinrymsha@gmail.com>
2023-08-18 20:54:22 +0300

    Initial Commit

---
+++ docusketchTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  docusketchTestTask
+//
+//  Created by Valentin on 18.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
